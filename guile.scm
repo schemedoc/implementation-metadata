af10bd99ca00be3d3b318de93433bf6e8d4af627
Lassi Kortela <lassi@lassi.io>
2019-04-26 11:16:45 +0300

    Add GNU Guile

---
+++ guile.scm
@@ -0,0 +1,38 @@
+(title "Guile")
+
+(homepage-url "https://www.gnu.org/software/guile/")
+(issue-tracker-url "https://debbugs.gnu.org/cgi/pkgreport.cgi?pkg=guile")
+
+(documentation
+ (title "Reference Manual")
+ (web-url "https://www.gnu.org/software/guile/manual/"))
+
+(documentation
+ (title "FAQ")
+ (web-url "https://www.gnu.org/software/guile/docs/faq/guile-faq.html"))
+
+(mailing-list
+ (name "user")
+ (list-address "guile-user" "gnu.org")
+ (archive-url "https://lists.gnu.org/archive/html/guile-user/")
+ (instructions-url "https://lists.gnu.org/mailman/listinfo/guile-user/"))
+
+(mailing-list
+ (name "devel")
+ (list-address "guile-devel" "gnu.org")
+ (archive-url "https://lists.gnu.org/archive/html/guile-devel/")
+ (instructions-url "https://lists.gnu.org/mailman/listinfo/guile-devel"))
+
+(version-control
+ (web-url "https://savannah.gnu.org/git/?group=guile")
+ (git-url "https://git.savannah.gnu.org/git/guile.git"))
+
+(release
+ (date (2018 7 2))
+ (version-number "2.2.4")
+ (source-archive
+  (url "https://ftp.gnu.org/gnu/guile/guile-2.2.4.tar.gz"))
+ (source-archive
+  (url "https://ftp.gnu.org/gnu/guile/guile-2.2.4.tar.lz"))
+ (source-archive
+  (url "https://ftp.gnu.org/gnu/guile/guile-2.2.4.tar.xz")))
