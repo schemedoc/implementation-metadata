ade8aaae398b7675b6f52d2029bc9f2ed822baa5
Lassi Kortela <lassi@lassi.io>
2019-04-26 15:11:13 +0300

    Add Gambit

---
+++ gambit.scm
@@ -0,0 +1,35 @@
+(title "Gambit")
+
+(homepage-url "http://dynamo.iro.umontreal.ca/wiki/index.php/Main_Page")
+(issue-tracker-url "https://github.com/gambit/gambit/issues")
+
+(documentation
+ (title "Manual")
+ (web-url "https://www.iro.umontreal.ca/~gambit/doc/gambit.html")
+ (pdf-url "https://www.iro.umontreal.ca/~gambit/doc/gambit.pdf"))
+
+(web-chat-url "https://gitter.im/gambit/gambit")
+
+(irc-channel
+ (channel "#gambit")
+ (network "irc.freenode.net")
+ (webchat "https://webchat.freenode.net/?channels=%23gambit"))
+
+(mailing-list
+ (name "gambit-list")
+ (list-address "gambit-list" "iro.umontreal.ca")
+ (archive-url "https://mailman.iro.umontreal.ca/pipermail/gambit-list/")
+ (instructions-url "https://mailman.iro.umontreal.ca/mailman/listinfo/gambit-list"))
+
+(version-control
+ (web-url "https://github.com/gambit/gambit")
+ (git-url "https://github.com/gambit/gambit.git")
+ (git-url "git@github.com:gambit/gambit.git"))
+
+(release
+ (date (2019 2 5))
+ (version-number "4.9.3")
+ (source-archive
+  (url "https://www-labs.iro.umontreal.ca/~gambit/download/gambit/v4.9/source/gambit-v4_9_3.tgz"))
+ (source-archive
+  (url "https://www-labs.iro.umontreal.ca/~gambit/download/gambit/v4.9/source/gambit-v4_9_3-devel.tgz")))
