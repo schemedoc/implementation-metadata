ecb1befb29045ed48ecb6ea3190ce206d98ef470
Lassi Kortela <lassi@lassi.io>
2019-04-26 14:48:36 +0300

    Add Ypsilon

---
+++ ypsilon.scm
@@ -0,0 +1,17 @@
+(title "Ypsilon")
+
+(homepage-url "http://www.littlewingpinball.com/doc/en/ypsilon/index.html")
+(issue-tracker-url "https://code.google.com/archive/p/ypsilon/issues")
+
+(documentation
+ (title "Assorted documents on Google Code")
+ (web-url "https://code.google.com/archive/p/ypsilon/wikis"))
+
+(version-control
+ (web-url "https://code.google.com/archive/p/ypsilon/source/default/source"))
+
+(release
+ (date (2008 12 23))
+ (version-number "0.9.6.update3")
+ (source-archive
+  (url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/ypsilon/ypsilon-0.9.6.update3.tar.gz")))
