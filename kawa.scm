a2fc68185efaf1b38deb8a1d62757f8f4a891069
Lassi Kortela <lassi@lassi.io>
2019-04-26 15:01:45 +0300

    Add Kawa

---
+++ kawa.scm
@@ -0,0 +1,31 @@
+(title "Kawa Scheme")
+
+(homepage-url "https://www.gnu.org/software/kawa/")
+(issue-tracker-url "https://gitlab.com/kashell/Kawa/issues")
+(issue-tracker-url "http://savannah.gnu.org/bugs/?group=kawa")
+
+(documentation
+ (title "Reference Documentation")
+ (web-url "https://www.gnu.org/software/kawa/pt01.html"))
+
+(documentation
+ (title "Tutorial")
+ (web-url "https://www.gnu.org/software/kawa/tutorial/"))
+
+(mailing-list
+ (name "kawa")
+ (list-address "kawa" "sourceware.org")
+ (archive-url "https://sourceware.org/ml/kawa/")
+ (instructions-url "https://www.gnu.org/software/kawa/Mailing-lists.html"))
+
+(version-control
+ (web-url "https://gitlab.com/kashell/Kawa")
+ (git-url "https://gitlab.com/kashell/Kawa.git")
+ (git-url "git@gitlab.com:kashell/Kawa.git"))
+
+(release
+ (date (2017 10 2))
+ (version-number "3.0")
+ (source-archive
+  (url "https://ftp.gnu.org/pub/gnu/kawa/kawa-3.0.tar.gz")
+  (hash sha1 "783fe0bacf6bfc147c470f6a3105adf7840f73ae")))
