3c889198b513c4f966dda3dac5acf2fb4929e833
Lassi Kortela <lassi@lassi.io>
2019-04-26 10:22:40 +0300

    Add draft of Chibi data

---
+++ chibi.scm
@@ -0,0 +1,25 @@
+(title "Chibi-Scheme")
+
+(homepage-url "http://synthcode.com/wiki/chibi-scheme")
+(issue-tracker-url "https://github.com/ashinn/chibi-scheme/issues")
+
+(mailing-list
+ (name "Google group")
+ (archive-url "https://groups.google.com/group/chibi-scheme"))
+
+(version-control
+ (web-url "https//github.com/ashinn/chibi-scheme")
+ (git-url "git@github.comashinn/chibi-scheme.git")
+ (git-url "https//github.com/ashinn/chibi-scheme.git"))
+
+(release
+ (date (2018 1 25))
+ (version-number "0.8.0")
+ (source-archive-url "http//synthcode.com/scheme/chibi/chibi-scheme-0.8.0.tgz")
+ (source-archive-hash sha1 "7ce5f6609e7a84cefaf4f7596de3770b8db4ac00")
+ (bundled-srfi-implementations
+  1 2 6 8 9 11 14 16 18 23
+  26 27 33 38 39 41 46 55 69 95
+  98 99 101 111 113 115 116 117 121 124
+  125 127 128 129 130 132 133 134 135 139
+  141 142 143 144 145 147 151 154 159))
