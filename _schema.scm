45de4acf0108510c0f5e425ea32ab87dba69eab0
Lassi Kortela <lassi@lassi.io>
2019-04-26 10:22:14 +0300

    Add draft Schema
    Does not use any formal schema definition language yet.

---
+++ _schema.scm
@@ -0,0 +1,22 @@
+(title <string>)
+
+(homepage-url <uri>)
+(issue-tracker-url <uri>)
+(community-wiki-url <uri>)
+(community-forum-url <uri>)
+
+(mailing-list
+ (name)
+ (archive-url <uri>)
+ (subscribe-url <uri>))
+
+(version-control
+ (web-url <uri>)
+ (git-url <uri>))
+
+(release
+ (date <yyyy-mm-dd>)
+ (version-number "0.8.0")
+ (source-archive-url <uri>)
+ (source-archive-sha1 <sha1>)
+ (bundled-srfi-implementations <nat...>))
