ed33f28fbdea2b095bc3d2e3779d0732dc16c44b
Lassi Kortela <lassi@lassi.io>
2019-04-26 10:49:46 +0300

    Add chicken

---
+++ chicken.scm
@@ -0,0 +1,41 @@
+(title "CHICKEN Scheme")
+
+(homepage-url "https://www.call-cc.org/")
+(issue-tracker-url "https://bugs.call-cc.org/")
+
+(documentation
+ (title "Manual (version 4)")
+ (web-url "https://wiki.call-cc.org/man/4/The%20User%27s%20Manual"))
+
+(documentation
+ (title "Manual (version 5)")
+ (web-url "https://wiki.call-cc.org/man/5/The%20User%27s%20Manual"))
+
+(mailing-list
+ (name "users")
+ (list-address "chicken-users" "nongnu.org")
+ (archive-url "https://lists.nongnu.org/archive/html/chicken-users/")
+ (instructions-url "https://lists.nongnu.org/mailman/listinfo/chicken-users"))
+
+(mailing-list
+ (name "hackers")
+ (list-address "chicken-hackers" "nongnu.org")
+ (archive-url "https://lists.nongnu.org/archive/html/chicken-hackers/")
+ (instructions-url "https://lists.nongnu.org/mailman/listinfo/chicken-hackers"))
+
+(mailing-list
+ (name "janitors")
+ (list-address "chicken-janitors" "nongnu.org")
+ (archive-url "https://lists.nongnu.org/archive/html/chicken-janitors/")
+ (instructions-url "https://lists.nongnu.org/mailman/listinfo/chicken-janitors"))
+
+(version-control
+ (web-url "https://code.call-cc.org/cgi-bin/gitweb.cgi?p=chicken-core.git")
+ (git-url "https://code.call-cc.org/git/chicken-core.git")
+ (git-url "git://code.call-cc.org/chicken-core"))
+
+(release
+ (date (2018 11 7))
+ (version-number "5.0.0")
+ (source-archive-url "https://code.call-cc.org/releases/5.0.0/chicken-5.0.0.tar.gz")
+ (source-archive-hash sha256 "a8b94bb94c5d6a4348cedd75dc334ac80924bcd9a7a7a3d6af5121e57ef66595"))
