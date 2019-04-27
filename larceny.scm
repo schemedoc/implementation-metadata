b0e194842475d09c5dca31366dfcb6f29f096250
Lassi Kortela <lassi@lassi.io>
2019-04-27 12:29:22 +0300

    Add Larceny

---
+++ larceny.scm
@@ -0,0 +1,26 @@
+(title "Larceny")
+
+(homepage-url "http://larcenists.org/")
+(issue-tracker-url "https://github.com/larcenists/larceny/issues")
+
+(documentation
+ (title "User's Manual")
+ (web-url "http://larcenists.org/Documentation/Documentation1.3/index.html"))
+
+(mailing-list
+ (name "users")
+ (list-address "larceny-users" "lists.ccs.neu.edu")
+ (archive-url "https://lists.ccs.neu.edu/pipermail/larceny-users/")
+ (instructions-url "https://lists.ccs.neu.edu/bin/listinfo/larceny-users"))
+
+(version-control
+ (web-url "https://github.com/larcenists/larceny")
+ (git-url "https://github.com/larcenists/larceny.git")
+ (git-url "git@github.com:larcenists/larceny.git"))
+
+(release
+ (date (2017 8 6))
+ (version-number "1.3")
+ (source-archive
+  (url "http://larcenists.org/LarcenyReleases/larceny-1.3-src.tar.gz")
+  (url "http://larcenists.org/LarcenyReleases/larceny-1.3-src.zip")))
