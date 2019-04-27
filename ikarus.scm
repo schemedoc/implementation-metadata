a100b1b8d80a6dd15040c0d1a5d7782f2f477eeb
Lassi Kortela <lassi@lassi.io>
2019-04-27 13:04:35 +0300

    Add more URLs for Ikarus

--- ikarus.scm
+++ ikarus.scm
@@ -1,10 +1,17 @@
 (title "Ikarus")
 
 (homepage-url "http://ikarus-scheme.org/")
+(homepage-url "https://launchpad.net/ikarus/")
+(issue-tracker-url "https://bugs.launchpad.net/ikarus")
+(community-forum-url "https://answers.launchpad.net/ikarus")
 
 (documentation
  (title "User's Guide")
  (web-url "http://ikarus-scheme.org/ikarus-scheme-users-guide.pdf"))
+
+(version-control
+ (web-url "https://code.launchpad.net/ikarus")
+ (bazaar-url "https://code.launchpad.net/~aghuloum/ikarus/ikarus.dev"))
 
 (release
  (date (2007 10 31))
