0802241300292aa1af4bd27529a5052541d8300d
Lassi Kortela <lassi@lassi.io>
2019-04-27 12:44:36 +0300

    Add Ikarus

---
+++ ikarus.scm
@@ -0,0 +1,22 @@
+(title "Ikarus")
+
+(homepage-url "http://ikarus-scheme.org/")
+
+(documentation
+ (title "User's Guide")
+ (web-url "http://ikarus-scheme.org/ikarus-scheme-users-guide.pdf"))
+
+(release
+ (date (2007 10 31))
+ (version-number "0.0.1")
+ (source-archive (url "http://ikarus-scheme.org/ikarus-0.0.1.tar.gz")))
+
+(release
+ (date (2007 11 29))
+ (version-number "0.0.2")
+ (source-archive (url "http://ikarus-scheme.org/ikarus-0.0.2.tar.gz")))
+
+(release
+ (date (2008 2 2))
+ (version-number "0.0.3")
+ (source-archive (url "http://ikarus-scheme.org/ikarus-0.0.3.tar.gz")))
