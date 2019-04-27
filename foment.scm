cb72fdc35cec3112753bda85d5d9e5d41f6c367e
Lassi Kortela <lassi@lassi.io>
2019-04-27 10:35:11 +0300

    Add Foment

---
+++ foment.scm
@@ -0,0 +1,29 @@
+(title "Foment")
+
+(homepage-url "https://github.com/leftmike/foment")
+(issue-tracker-url "https://github.com/leftmike/foment/issues")
+
+(documentation
+ (title "Wiki")
+ (web-url "https://github.com/leftmike/foment/wiki/Foment"))
+
+(version-control
+ (web-url "https://github.com/leftmike/foment")
+ (git-url "https://github.com/leftmike/foment.git")
+ (git-url "git@github.com:leftmike/foment.git"))
+
+(release
+ (date (2014 3 29))
+ (version-number "0.4")
+ (source-archive
+  (url "https://github.com/leftmike/foment/archive/v0.4.zip"))
+ (source-archive
+  (url "https://github.com/leftmike/foment/archive/v0.4.tar.gz")))
+
+(release
+ (date (2014 4 2))
+ (version-number "0.4.1")
+ (source-archive
+  (url "https://github.com/leftmike/foment/archive/v0.4.1.zip"))
+ (source-archive
+  (url "https://github.com/leftmike/foment/archive/v0.4.1.tar.gz")))
