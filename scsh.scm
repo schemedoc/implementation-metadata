041fb0345f8aa643e27c25d92727ea9a04835796
Lassi Kortela <lassi@lassi.io>
2019-04-27 10:59:53 +0300

    Add Scsh

---
+++ scsh.scm
@@ -0,0 +1,28 @@
+(title "Scsh")
+
+(homepage-url "https://scsh.net/")
+
+(documentation
+ (title "Reference Manual")
+ (web-url "https://scsh.net/docu/html/man.html"))
+
+(irc-channel
+ (channel "#scsh")
+ (network "irc.freenode.net")
+ (webchat "https://webchat.freenode.net/?channels=%23scsh"))
+
+(mailing-list
+ (name "users")
+ (list-address "scsh-users" "scsh.net")
+ (instructions-url "https://scsh.net/support.html"))
+
+(mailing-list
+ (name "hackers")
+ (list-address "scsh-hackers" "scsh.net")
+ (instructions-url "https://scsh.net/support.html"))
+
+(release
+ (date (2006 5 16))
+ (version-number "0.6.7")
+ (source-archive
+  (url "https://ftp.scsh.net/pub/scsh/0.6/scsh-0.6.7.tar.gz")))
