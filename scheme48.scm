5ef568a600ed4c712b8043b280ee5584bbb5478f
Lassi Kortela <lassi@lassi.io>
2019-04-27 10:54:04 +0300

    Add Scheme 48

---
+++ scheme48.scm
@@ -0,0 +1,27 @@
+(title "Scheme 48")
+
+(homepage-url "http://www.s48.org/")
+
+(documentation
+ (title "Reference Manual")
+ (web-url "http://www.s48.org/1.9.2/manual/manual.html")
+ (pdf-url "http://www.s48.org/1.9.2/s48manual.pdf"))
+
+(mailing-list
+ (name "scheme-48")
+ (list-address "scheme-48" "s48.org")
+ (instructions-url "http://www.s48.org/mailing-list.html"))
+
+(version-control
+ (web-url "http://www.s48.org/cgi-bin/hgwebdir.cgi/s48")
+ (mercurial-url "http://www.s48.org/cgi-bin/hgwebdir.cgi/s48"))
+
+(version-control
+ (web-url "http://www.s48.org/cgi-bin/hgwebdir.cgi/s48-stable")
+ (mercurial-url "http://www.s48.org/cgi-bin/hgwebdir.cgi/s48-stable"))
+
+(release
+ (date (2014 4 12))
+ (version-number "1.9.2")
+ (source-archive
+  (url "http://www.s48.org/1.9.2/scheme48-1.9.2.tgz")))
