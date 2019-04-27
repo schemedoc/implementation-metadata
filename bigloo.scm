3b37f2785575d52703cb263a97136c6716b0d6fa
Lassi Kortela <lassi@lassi.io>
2019-04-27 12:38:21 +0300

    Add Bigloo

---
+++ bigloo.scm
@@ -0,0 +1,21 @@
+(title "Bigloo")
+
+(homepage-url "https://www-sop.inria.fr/mimosa/fp/Bigloo/")
+
+(documentation
+ (title "User manual")
+ (web-url "https://www-sop.inria.fr/mimosa/fp/Bigloo/doc/bigloo.html"))
+
+(community-wiki-url "http://bigloo.pbworks.com/w/")
+
+(mailing-list
+ (name "bigloo")
+ (list-address "bigloo" "sophia.inria.fr")
+ (archive-url "https://sympa.inria.fr/sympa/arc/bigloo")
+ (instructions-url "https://www-sop.inria.fr/mimosa/fp/Bigloo/bigloo.html#Mailing-list"))
+
+(release
+ (date (2018 12 18))
+ (version-number "4.3e")
+ (source-archive
+  (url "ftp://ftp-sop.inria.fr/indes/fp/Bigloo/bigloo4.3e.tar.gz")))
