fb312766bb958c26f25f123746bd0d3070a28a05
Lassi Kortela <lassi@lassi.io>
2019-04-27 10:43:29 +0300

    Add IronScheme

---
+++ ironscheme.scm
@@ -0,0 +1,19 @@
+(title "IronScheme")
+
+(homepage-url "https://github.com/leppie/IronScheme")
+(homepage-url "https://archive.codeplex.com/?p=ironscheme")
+(issue-tracker-url "https://github.com/leppie/IronScheme/issues")
+
+(irc-channel
+ (channel "#IronScheme")
+ (network "irc.freenode.net")
+ (webchat "https://webchat.freenode.net/?channels=%23IronScheme"))
+
+(social-media-profile "https://twitter.com/IronScheme")
+
+(web-repl "http://eval.ironscheme.net/")
+
+(version-control
+ (web-url "https://github.com/leppie/IronScheme")
+ (git-url "https://github.com/leppie/IronScheme.git")
+ (git-url "git@github.com:leppie/IronScheme.git"))
