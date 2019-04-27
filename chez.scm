e39ce3bd3e3f5a51d23c1407360a62a94b84091f
Lassi Kortela <lassi@lassi.io>
2019-04-27 11:18:55 +0300

    Add Chez

---
+++ chez.scm
@@ -0,0 +1,32 @@
+(title "Chez Scheme")
+
+(homepage-url "https://www.scheme.com/")
+(homepage-url "https://cisco.github.io/ChezScheme/")
+(wikipedia-url "https://en.wikipedia.org/wiki/Chez_Scheme")
+(issue-tracker-url "https://github.com/cisco/ChezScheme/issues")
+
+(documentation
+ (title "User's Guide")
+ (web-url "http://cisco.github.io/ChezScheme/csug9.5/csug.html"))
+
+(irc-channel
+ (channel "#chez")
+ (network "irc.freenode.net")
+ (webchat "https://webchat.freenode.net/?channels=%23chez"))
+
+(mailing-list
+ (name "Google group")
+ (archive-url "https://groups.google.com/group/chez-scheme"))
+
+(version-control
+ (web-url "https://github.com/cisco/ChezScheme")
+ (git-url "https://github.com/cisco/ChezScheme.git")
+ (git-url "git@github.com:cisco/ChezScheme.git"))
+
+(release
+ (date (2019 3 22))
+ (version-number "9.5.2")
+ (source-archive
+  (url "https://github.com/cisco/ChezScheme/archive/v9.5.2.zip"))
+ (source-archive
+  (url "https://github.com/cisco/ChezScheme/archive/v9.5.2.tar.gz")))
