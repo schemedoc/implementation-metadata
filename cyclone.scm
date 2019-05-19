535b6314996d55e2d3898aff3b6de4454855d981
Justin Ethier <justin.ethier@gmail.com>
2019-05-18 22:55:17 -0400

    Create cyclone.scm

---
+++ cyclone.scm
@@ -0,0 +1,26 @@
+(title "Cyclone Scheme")
+
+(homepage-url "http://justinethier.github.io/cyclone/")
+(issue-tracker-url "https://github.com/justinethier/cyclone/issues")
+
+(documentation
+ (title "User Manual")
+ (web-url "http://justinethier.github.io/cyclone/docs/User-Manual"))
+
+(documentation
+ (title "API Documentation")
+ (web-url "http://justinethier.github.io/cyclone/docs/API"))
+
+(package
+ (manager arch-linux-aur)
+ (name "cyclone-scheme"))
+
+(version-control
+ (web-url "https://github.com/justinethier/cyclone")
+ (git-url "https://github.com/justinethier/cyclone.git"))
+
+(release
+ (date (2019 4 15))
+ (version-number "0.11")
+ (source-archive
+  (url "https://github.com/justinethier/cyclone-bootstrap/archive/v0.11.tar.gz")))
