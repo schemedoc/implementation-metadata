f14743e3b26f786ff099425ba4091977cd298e1e
Lassi Kortela <lassi@lassi.io>
2019-06-28 14:07:55 +0300

    Add BiwaScheme

---
+++ biwa.scm
@@ -0,0 +1,21 @@
+(title "BiwaScheme")
+
+(homepage-url "https://www.biwascheme.org/")
+(issue-tracker-url "https://github.com/biwascheme/biwascheme/issues")
+
+(documentation
+ (title "Reference")
+ (web-url "https://www.biwascheme.org/doc/reference.html"))
+
+(documentation
+ (title "Other documentation")
+ (web-url "https://www.biwascheme.org/doc/"))
+
+(package
+ (manager npm)
+ (name "https://www.npmjs.com/package/biwascheme"))
+
+(version-control
+ (web-url "https://github.com/biwascheme/biwascheme")
+ (git-url "https://github.com/biwascheme/biwascheme.git")
+ (git-url "git@github.com:biwascheme/biwascheme.git"))
