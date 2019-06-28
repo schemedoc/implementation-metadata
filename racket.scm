75057ae69b5cba345eb961e445bf3e779d420327
Lassi Kortela <lassi@lassi.io>
2019-06-28 13:00:30 +0300

    Start metadata for Racket

---
+++ racket.scm
@@ -0,0 +1,8 @@
+(title "Racket")
+
+(homepage-url "https://racket-lang.org/")
+(wikipedia-url "https://en.wikipedia.org/wiki/Racket_%28programming_language%29")
+
+(mailing-list
+ (name "Google group")
+ (archive-url "https://groups.google.com/group/racket-users"))
