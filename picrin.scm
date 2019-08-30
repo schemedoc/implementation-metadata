915aed1a1d6d1a4ec954f8820d4d2f826220a909
Lassi Kortela <lassi@lassi.io>
2019-08-30 22:34:21 +0300

    Open files on Schemik and Picrin

---
+++ picrin.scm
@@ -0,0 +1,11 @@
+(id picrin)
+(title "Picrin")
+
+(version-control
+ (web-url "https://github.com/picrin-scheme/picrin")
+ (git-url "https://github.com/picrin-scheme/picrin.git")
+ (git-url "git@github.com:picrin-scheme/picrin.git"))
+
+
+(pseudo-release
+ (source-checkout
---
+++ schemik.scm
@@ -0,0 +1,10 @@
+(id schemik)
+(title "Schemik")
+
+(homepage-url "http://schemik.sourceforge.net/")
+
+(features
+ implicitly-parallel
+ first-class-macros
+ software-transactional-memory
+ just-in-time-compile)
