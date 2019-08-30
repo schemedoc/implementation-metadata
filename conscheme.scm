15d224806dbe9f7699cb811fd4fd27e317fd55c6
Lassi Kortela <lassi@lassi.io>
2019-08-30 22:36:43 +0300

    Open a file on Conscheme

---
+++ conscheme.scm
@@ -0,0 +1,9 @@
+(id conscheme)
+(title "Conscheme")
+
+(implementation-languages go scheme)
+
+(version-control
+ (web-url "https://github.com/weinholt/conscheme")
+ (git-url "https://github.com/weinholt/conscheme.git")
+ (git-url "git@github.com:weinholt/conscheme.git"))
