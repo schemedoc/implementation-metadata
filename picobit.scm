e13fb5ee83fb18f4a89a14ce55f0d6e4ce7ed005
Lassi Kortela <lassi@lassi.io>
2019-09-03 17:31:59 +0300

    Add PICOBIT

---
+++ picobit.scm
@@ -0,0 +1,15 @@
+(id picobit)
+(title "PICOBIT")
+
+(person "Marc Feeley")
+(person "Vincent St-Amour")
+(organization "Université de Montréal")
+
+(paper
+ (type inproceedings)
+ (title "PICOBIT: a compact Scheme system for microcontrollers")
+ (author "Vincent St-Amour")
+ (author "Marc Feeley")
+ (booktitle "International Symposium on Implementation and Application of Functional Languages")
+ (year "2009")
+ (url "https://www.iro.umontreal.ca/~feeley/papers/StAmourFeeleyIFL09.pdf"))
