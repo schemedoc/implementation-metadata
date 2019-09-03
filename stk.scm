60349b5224e201525507c197a827ae82626543f4
Lassi Kortela <lassi@lassi.io>
2019-09-03 15:04:21 +0300

    Open a file for STk

---
+++ stk.scm
@@ -0,0 +1,28 @@
+(id stk)
+(title "STk")
+
+(homepage-url "http://kaolin.unice.fr/STk/")
+
+(documentation
+ (title "Reference Manual (4.0.0)")
+ (web-url "http://kaolin.unice.fr/STk/Doc/STk.pdf"))
+
+(documentation
+ (title "FAQ")
+ (web-url "http://kaolin.unice.fr/STk/FAQ/"))
+
+(release
+ (date "1996-09-26")
+ (archive "http://kaolin.unice.fr/STk/Distrib/STk-3.1.1.tar.gz"))
+
+(release
+ (date "1999-02-02")
+ (archive "http://kaolin.unice.fr/STk/Distrib/STk-3.99.4.tar.gz"))
+
+(release
+ (date "1999-09-05")
+ (archive "http://kaolin.unice.fr/STk/Distrib/STk-4.0.0.tar.gz"))
+
+(release
+ (date "1999-09-27")
+ (archive "http://kaolin.unice.fr/STk/Distrib/STk-4.0.1.tar.gz"))
