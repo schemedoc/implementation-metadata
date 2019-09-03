09990d4e68e2d06f9cfd14509a0779024f3a4f87
Lassi Kortela <lassi@lassi.io>
2019-09-03 15:52:02 +0300

    Add UCB Scheme

---
+++ ucb.scm
@@ -0,0 +1,14 @@
+(id ucb)
+(title "UCB Scheme")
+
+(homepage-url "https://inst.eecs.berkeley.edu/~scheme/")
+
+(documentation
+ (title "FAQ")
+ (web-url "https://inst.eecs.berkeley.edu/~scheme/faq.html"))
+
+(release
+ (date "2010-08-02")
+ (version "1.3.6")
+ (derived-from stk "4.0.1")
+ (url "https://inst.eecs.berkeley.edu/~scheme/source/stk-1.3.6.tgz"))
