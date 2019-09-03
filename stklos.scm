c3ae621d6ae269ff98454b37ccd4bfb6017f5877
Lassi Kortela <lassi@lassi.io>
2019-09-03 14:36:10 +0300

    Open a file for STklos

---
+++ stklos.scm
@@ -0,0 +1,14 @@
+(id stklos)
+(title "STklos")
+(long-title "STklos Scheme")
+
+(homepage-url "https://www.stklos.net/")
+(github "egallesio/STklos")
+(issue-tracker github)
+
+(person "Erick Gallesio")
+
+(release
+ (version "1.31")
+ (date "2019-07-13")
+ (archive "https://www.stklos.net/download/stklos-1.31.tar.gz"))
