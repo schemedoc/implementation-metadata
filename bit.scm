646a1b0be17668593dd487f9bb6cc6b54c5b067d
Lassi Kortela <lassi@lassi.io>
2019-09-03 16:06:40 +0300

    Add BIT

---
+++ bit.scm
@@ -0,0 +1,29 @@
+(id bit)
+(title "BIT")
+
+(organization "Universite de Montreal")
+(person "Danny Dubé")
+(person "Marc Feeley")
+
+(paper
+ (title "Un système de programmation Scheme pour micro-contrôleur")
+ (author "Danny Dubé"))
+
+(paper
+ (title "Un GC temps re'el semi-compactant")
+ (author "Danny Dubé")
+ (author "Marc Feeley")
+ (author "Manuel Serrano")
+ (date "1996"))
+
+(paper
+ (title "BIT: A Very Compact Scheme System for Microcontrollers")
+ (author "Danny Dubé")
+ (author "Marc Feeley")
+ (url "https://www2.ift.ulaval.ca/~dadub100/files/HOSC.pdf")
+ (date "2005"))
+
+(release
+ (date "1995")
+ (url "http://web.archive.org/web/20060708102643/http://www.iro.umontreal.ca/~dube/bit.tar.gz")
+ (sha256 "082a2178254ee06b730396b6aea60870cf56c851694365a2126808b073e4b03e"))
