7cd6e436e7da2f563a3cb1e34c2e43a7cb6516a6
Lassi Kortela <lassi@lassi.io>
2019-09-03 17:28:33 +0300

    Add PICBIT

---
+++ picbit.scm
@@ -0,0 +1,17 @@
+(id picbit)
+(title "PICBIT")
+
+(person "Marc Feeley")
+(person "Danny Dubé")
+(organization "Université de Montréal")
+(organization "Université Laval")
+
+(paper
+ (type inproceedings)
+ (title "PICBIT: A Scheme system for the PIC microcontroller")
+ (author "Marc Feeley")
+ (author "Danny Dubé")
+ (booktitle "Proceedings of the Fourth Workshop on Scheme and Functional Programming")
+ (year "2003")
+ (url "https://www.iro.umontreal.ca/~feeley/papers/FeeleyDubeSW03.pdf")
+ (url "https://www2.ift.ulaval.ca/~dadub100/files/Scheme03.pdf"))
