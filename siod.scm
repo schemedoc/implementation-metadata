d13d699c64a244895ae5efb273b78ba5e2399459
Lassi Kortela <lassi@lassi.io>
2019-09-09 17:27:27 +0300

    Add SIOD

---
+++ siod.scm
@@ -0,0 +1,9 @@
+(id siod)
+(title "SIOD")
+(long-title "Scheme In One Defun")
+
+(homepage-url "https://people.delphiforums.com/gjc/siod.html")
+(codeplex "siod")
+(wikipedia "SIOD")
+
+(person "George J. Carrette")
