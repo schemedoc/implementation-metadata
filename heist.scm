354bad86169829e929c1168d5272fb75caca3854
Lassi Kortela <lassi@lassi.io>
2019-09-09 20:08:34 +0300

    Add Heist

---
+++ heist.scm
@@ -0,0 +1,12 @@
+(id heist)
+(title "Heist")
+(tagline "Scheme in as little Ruby and as much Scheme as possible")
+
+(github "jcoglan/heist")
+
+(features
+ continuations
+ lazy-evaluation
+ ruby
+ syntax-rules
+ tail-recursion)
