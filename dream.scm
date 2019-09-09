dd7f800935e0f58adcd63ec52b48fe504bdea2b4
Lassi Kortela <lassi@lassi.io>
2019-09-09 21:31:17 +0300

    Add Dream

---
+++ dream.scm
@@ -0,0 +1,12 @@
+(id dream)
+(title "Dream")
+(long-title "The 'dream' Scheme Interpreter")
+(tagline "R4RS interpreter written in Assembler for Linux")
+
+(homepage-url "http://web.archive.org/web/20070929205658/http://www.stripedgazelle.org/joey/dream.html")
+
+(person "David Joseph Stith")
+
+(features
+ assembler
+ r4rs)
