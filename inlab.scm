0c556287d0dd80f70269d943b9d41ec2f31f26bb
Lassi Kortela <lassi@lassi.io>
2019-09-09 17:20:32 +0300

    Add Inlab

---
+++ inlab.scm
@@ -0,0 +1,9 @@
+(id inlab)
+(short-title "Inlab")
+(title "Inlab Scheme")
+(tagline "complete R4RS implementation for image processing")
+
+(person "Thomas Obermair")
+(organization "Inlab Networks")
+
+(homepage "https://www.inlab.net/inlab-scheme/")
