aa0aecf73dfe5bcd6a6f1feff8933a110cf6b3c1
Lassi Kortela <lassi@lassi.io>
2019-09-09 20:03:53 +0300

    Add HScheme

---
+++ hscheme.scm
@@ -0,0 +1,14 @@
+(id hscheme)
+(title "HScheme")
+(tagline "Guile for Haskell")
+
+(sourceforge "hscheme")
+
+(person "Ashley Yakeley")
+
+(features
+ full-numeric-tower
+ haskell
+ interpreter
+ r5rs
+ syntax-rules)
