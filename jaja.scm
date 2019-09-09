d12bf99a03307b30b4bb17eefccca8dac5329cc8
Lassi Kortela <lassi@lassi.io>
2019-09-09 19:20:23 +0300

    Add Jaja and PS3I

---
+++ jaja.scm
@@ -0,0 +1,13 @@
+(id jaja)
+(title "Jaja")
+(tagline "Scheme in Java")
+
+(homepage-url "https://pages.lip6.fr/Christian.Queinnec/Java/Jaja.html")
+
+(person "Christian Queinnec")
+(organization "Université Paris 6")
+(organization "École Polytechnique")
+
+(features
+ jvm
+ r4rs)
---
+++ ps3i.scm
@@ -0,0 +1,13 @@
+(id ps3i)
+(title "PS3I")
+(long-title "Persistent Server-Side Scheme Interpreter")
+(tagline "multi-user, multi-threaded, servlet R4RS implementation in Java")
+
+(homepage-url "https://pages.lip6.fr/Christian.Queinnec/VideoC/ps3i.html")
+
+(person "Christian Queinnec")
+(organization "Université Paris 6")
+
+(features
+ jvm
+ r4rs)
