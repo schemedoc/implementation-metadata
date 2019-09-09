7b0f95f0294db9771784051f2b6c3f3f2e85d3ba
Lassi Kortela <lassi@lassi.io>
2019-09-09 18:00:48 +0300

    Add Viivi

---
+++ viivi.scm
@@ -0,0 +1,14 @@
+(id viivi)
+(title "Viivi")
+(tagline "complete R5RS interpreter for the JVM")
+
+(homepage-url "http://home.j00.itscom.net/ilma/english/viivi/")
+(person "Ilma")
+
+(features
+ case-sensitive-symbols
+ debug-trace
+ evaluation-order-left-to-right
+ full-continuations
+ jvm
+ r5rs)
