da46357c23b02558bc00ab5b85275e02303219e5
Lassi Kortela <lassi@lassi.io>
2019-09-09 21:13:27 +0300

    Add nanoScheme

---
+++ nano.scm
@@ -0,0 +1,7 @@
+(id nano)
+(title "nanoScheme")
+(tagline "a tiny scheme interpreter, written in Delphi as a Windows console app")
+
+(homepage-url "http://www.spf-15.com/nano.html")
+
+(person "Shawn Flisakowski")
