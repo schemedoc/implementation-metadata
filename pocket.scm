7ca66548dcc99e3e65a81457bf2a0ddb657af8bf
Lassi Kortela <lassi@lassi.io>
2019-09-09 20:51:05 +0300

    Add Pocket Scheme

---
+++ pocket.scm
@@ -0,0 +1,9 @@
+(id pocket)
+(short-title "Pocket")
+(title "Pocket Scheme")
+(tagline "R4RS Scheme for Windows CE-based Pocket PCs, smartphones and handheld PCs")
+(based-on "siod")
+
+(homepage-url "http://www.mazama.net/scheme/pscheme.htm")
+
+(person "Ben Goetter")
