ea877154e291ea6c7660f2631c6fd84232205596
Lassi Kortela <lassi@lassi.io>
2019-09-09 21:26:41 +0300

    Add Scheme 9

---
+++ scheme9.scm
@@ -0,0 +1,11 @@
+(id scheme9)
+(short-title "S9fES")
+(title "Scheme 9")
+(long-title "Scheme 9 from Empty Space")
+
+(homepage-url "https://www.t3x.org/s9fes/")
+
+(person "Nils M Holm")
+
+(features
+ r4rs)
