90df911bb192f0f8f183f327004c852b10eeb402
Lassi Kortela <lassi@lassi.io>
2019-09-09 18:01:35 +0300

    Add XScheme

---
+++ xscheme.scm
@@ -0,0 +1,19 @@
+(id xscheme)
+(title "XScheme")
+(tagline "object-oriented Scheme from the author of XLisp")
+
+(person "David Betz")
+
+(usenet "comp.lang.lisp.x")
+
+(features
+ object-system)
+
+(release
+ (version "0.20")
+ (platforms "Amiga")
+ (collection "comp.sources.amiga volume 90"))
+
+(release
+ (version "0.28")
+ (platforms "IBM PC" "Macintosh" "Atari" "Amiga"))
