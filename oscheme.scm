7cd99cf21b86c8f44c7e19b104a626c1668f0993
Lassi Kortela <lassi@lassi.io>
2019-09-09 17:29:37 +0300

    Add Oscheme

---
+++ oscheme.scm
@@ -0,0 +1,11 @@
+(id oscheme)
+(title "Oscheme")
+(long-title "Objective Scheme")
+(tagline "an embeddable mostly-R4RS interpreter with lots of extensions")
+
+(person "Anselm Baird-Smith")
+(organization "Groupe Bull")
+(organization "INRIA")
+
+(features
+ apropos)
