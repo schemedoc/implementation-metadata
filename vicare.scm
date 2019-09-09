18254381f5bde2b3ebd45122938cd98a4ef42dbe
Lassi Kortela <lassi@lassi.io>
2019-09-09 17:48:04 +0300

    Add Vicare

---
+++ vicare.scm
@@ -0,0 +1,18 @@
+(id vicare)
+(short-title "Vicare")
+(title "Vicare Scheme")
+(based-on "ikarus")
+
+(homepage-url "http://marcomaggi.github.io/vicare.html")
+(github "marcomaggi/vicare")
+(bitbucket "marcomaggi/vicare-scheme")
+
+(person "Marco Maggi")
+
+(features
+ 32-bit
+ 64-bit
+ native-code-compiler
+ r6rs
+ libgmp
+ libffi)
