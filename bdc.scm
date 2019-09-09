dbe451b042c36418a42af4c15651960a3297f738
Lassi Kortela <lassi@lassi.io>
2019-09-09 18:27:24 +0300

    Add BDC Scheme

---
+++ bdc.scm
@@ -0,0 +1,19 @@
+(id bdc)
+(short-title "BDC")
+(title "BDC Scheme")
+(tagline "fast interpreter for the JVM")
+
+(homepage-url "https://carlstrom.com/bdc-scheme/")
+
+(person "Brian D. Carlstrom")
+(organization "Massachusetts Institute of Technology")
+
+(features
+ jvm)
+
+(documentation
+ (title "Thesis")
+ (web-url "https://carlstrom.com/publications/meng_thesis.pdf"))
+
+(version-control
+ (git-url "https://carlstrom.com/bdc-scheme/bdc-scheme.git"))
