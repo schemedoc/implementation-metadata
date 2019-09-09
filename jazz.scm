4450397f161839025925972373cb2985b7611f16
Lassi Kortela <lassi@lassi.io>
2019-09-09 16:26:21 +0300

    Add JazzScheme

---
+++ jazz.scm
@@ -0,0 +1,16 @@
+(id jazz)
+(title "JazzScheme")
+(tagline "")
+
+(based-on "gambit")
+
+(homepage-url "http://www.jazzscheme.org/")
+
+(features
+ module-system
+ hygienic-macros
+ object-oriented-programming
+ cross-platform-gui
+ ide
+ build-system
+ self-contained-executables)
