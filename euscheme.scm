bc6782d596568a0767a2c4d32aa92c84109a3eb6
Lassi Kortela <lassi@lassi.io>
2019-09-09 22:10:05 +0300

    Add Euscheme

---
+++ euscheme.scm
@@ -0,0 +1,11 @@
+(id euscheme)
+(title "Euscheme")
+(tagline "an implementation of EuLisp level 0 (mostly)")
+(based-on "xscheme")
+
+(homepage-url "https://people.bath.ac.uk/masrjb/Sources/")
+
+(person "Russell Bradford")
+(person "Dave DeRoure")
+(person "Stewart Brodie")
+(organization "University of Bath")
