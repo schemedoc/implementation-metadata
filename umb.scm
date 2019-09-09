8c3c730c82b207b2617915237fb6549ba016b67c
Lassi Kortela <lassi@lassi.io>
2019-09-09 17:55:06 +0300

    Add UMB Scheme

---
+++ umb.scm
@@ -0,0 +1,13 @@
+(id umb)
+(short-title "UMB")
+(title "UMB Scheme")
+(tagline "R4RS Scheme system with editor and debugger")
+
+(person "William Campbell")
+(organization "University of Massachusetts at Boston")
+
+(features
+ debugger
+ editor
+ ieee-scheme
+ r4rs)
