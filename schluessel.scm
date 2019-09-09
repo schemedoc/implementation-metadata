55eb18979cab7771c33f0c52b397b24a082aa2f8
Lassi Kortela <lassi@lassi.io>
2019-09-09 22:00:59 +0300

    Add Schluessel

---
+++ schluessel.scm
@@ -0,0 +1,11 @@
+(id schluessel)
+(title "Schluessel")
+(tagline "Java/JavaScript R5RS implementation with lots of extras")
+
+(homepage-url "http://www.schluessel.jp/")
+
+(person "Yuichiro Moriguchi")
+
+(features
+ jvm
+ r5rs)
