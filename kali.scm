4a1d53a870e2f516798e3f215d6b5a3e1be01513
Lassi Kortela <lassi@lassi.io>
2019-09-09 22:19:43 +0300

    Add Kali Scheme

---
+++ kali.scm
@@ -0,0 +1,25 @@
+(id kali)
+(short-title "Kali")
+(title "Kali Scheme")
+(tagline "distributed Scheme with efficient transmission of higher-order objects such as closures and continuations")
+
+(homepage-url "http://www.neci.nj.nec.com/PLS/Kali.html")
+
+(person "Richard Kelsey")
+(person "Jonathan Rees")
+(organization "NEC Research Institute")
+
+(paper
+ (type article)
+ (title "Higher-order distributed objects")
+ (author "Henry Cejtin")
+ (author "Suresh Jagannathan")
+ (author "Richard Kelsey")
+ (journal "ACM Transactions on Programming Languages and Systems (TOPLAS)")
+ (volume "17")
+ (number "5")
+ (year "1995"))
+
+;; http://wiki.c2.com/?KaliScheme
+;; http://community.schemewiki.org/cgi-bin/scheme.cgi?kali-scheme-revival
+;; https://github.com/tonyg/kali-scheme
