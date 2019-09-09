ed7186c40c6c5ba826cbbca98f4919ccdf871b70
Lassi Kortela <lassi@lassi.io>
2019-09-09 20:20:55 +0300

    Add Owl Lisp and Otus Lisp

---
+++ otus.scm
@@ -0,0 +1,14 @@
+(id otus)
+(short-title "Otus")
+(title "Otus Lisp")
+(tagline "almost purely functional dialect of Lisp")
+(based-on owl)
+
+(github "otus-lisp/otus")
+
+(person "Yuriy Chumak")
+
+(features
+ compile-to-c
+ interpreter
+ r7rs)
---
+++ owl.scm
@@ -0,0 +1,21 @@
+(id owl)
+(short-title "Owl")
+(title "Owl Lisp")
+(tagline "a functional Scheme for world domination")
+
+(homepage-url "https://haltp.org/n/owl")
+(gitlab "owl-lisp/owl")
+(github "owl-lisp/owl")
+
+(person "Aki Helin")
+
+(package
+ (manager freebsd-ports)
+ (directory "lang")
+ (name "owl-lisp"))
+
+(features
+ compile-to-c
+ interpreter
+ purely-functional
+ r7rs)
