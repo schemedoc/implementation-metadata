9e724eb62b15740bdc0a4bc104f401073112e866
Lassi Kortela <lassi@lassi.io>
2019-09-09 18:34:13 +0300

    Add CPSCM Scheme

---
+++ cpscm.scm
@@ -0,0 +1,16 @@
+(id cpscm)
+(short-title "CPSCM")
+(title "CPSCM Scheme")
+(tagline "JavaScript and Common Lisp compiler based on CPS conversion")
+
+(homepage-url "https://www.omnigia.com/scheme/cpscm/home/")
+(google-code "cpscm")
+
+(person "Dan Muresan")
+(organization "Omnigia")
+
+(features
+ compile-to-common-lisp
+ compile-to-javascript
+ cps-conversion
+ r5rs)
