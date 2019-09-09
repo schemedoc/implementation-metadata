6cb9d37ec2d33ddd48eafedf73cb89a3bcfa6f3b
Lassi Kortela <lassi@lassi.io>
2019-09-09 17:44:42 +0300

    Add SigScheme

---
+++ sigscheme.scm
@@ -0,0 +1,14 @@
+(id sigscheme)
+(title "SigScheme")
+(tagline "64K embedded R5RS interpreter with multibyte characters")
+
+(google-code "sigscheme")
+
+(package
+ (manager debian)
+ (name "sigscheme"))
+
+(package
+ (manager gentoo)
+ (directory "dev-scheme")
+ (name "sigscheme"))
