f274320012424adc2528cb4965e5832db416ae00
Lassi Kortela <lassi@lassi.io>
2019-09-09 19:26:46 +0300

    Say PS3I is based on Jaja

--- ps3i.scm
+++ ps3i.scm
@@ -2,6 +2,7 @@
 (title "PS3I")
 (long-title "Persistent Server-Side Scheme Interpreter")
 (tagline "multi-user, multi-threaded, servlet R4RS implementation in Java")
+(based-on jaja)
 
 (homepage-url "https://pages.lip6.fr/Christian.Queinnec/VideoC/ps3i.html")
 
