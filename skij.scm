4ad9b94e16d70f9608166082e6fa97d0421615fa
Lassi Kortela <lassi@lassi.io>
2019-09-09 18:38:22 +0300

    Add Skij

---
+++ skij.scm
@@ -0,0 +1,12 @@
+(id skij)
+(title "Skij")
+(long-title "Scheme in Java")
+(tagline "Scheme in Java for interactive debugging and scripting")
+
+(person "Michael Travers")
+(organization "IBM Watson Labs")
+
+(homepage-url "https://alumni.media.mit.edu/~mt/skij/")
+
+(features
+ jvm)
