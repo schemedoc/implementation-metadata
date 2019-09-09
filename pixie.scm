d060bf76346a5bd8ad2ad8bf4ff439b4be2a8745
Lassi Kortela <lassi@lassi.io>
2019-09-09 18:10:25 +0300

    Add Pixie and Wraith

---
+++ pixie.scm
@@ -0,0 +1,9 @@
+(id pixie)
+(short-title "Pixie")
+(title "Pixie Scheme")
+
+(homepage-url "http://jayreynoldsfreeman.com/My/Software.html")
+
+(person "Jay Reynolds Freeman")
+
+(features)
---
+++ wraith.scm
@@ -0,0 +1,14 @@
+(id wraith)
+(short-title "Wraith")
+(title "Wraith Scheme")
+(based-on pixie)
+
+(homepage-url "http://jayreynoldsfreeman.com/My/Software.html")
+
+(person "Jay Reynolds Freeman")
+
+(features
+ 32-bit
+ 64-bit
+ mac-os-x
+ r5rs)
