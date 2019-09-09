07bb57042182223c1683c5f55cc769f0c22aa7cc
Lassi Kortela <lassi@lassi.io>
2019-09-09 17:05:39 +0300

    Add Armpit Scheme

---
+++ armpit.scm
@@ -0,0 +1,6 @@
+(id armpit)
+(short-title "Armpit")
+(title "Armpit Scheme")
+(tagline "a Scheme interpreter written in ARM assembly language for education, robotics and wearable devices")
+
+(sourceforge armpit)
