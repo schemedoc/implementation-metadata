29243966544623f031af5cacccc43a673d9ac5f0
Lassi Kortela <lassi@lassi.io>
2019-09-09 18:15:57 +0300

    Add LispMe

---
+++ lispme.scm
@@ -0,0 +1,13 @@
+(id lispme)
+(title "LispMe")
+(tagline "Scheme interpreter for Palm OS PDAs")
+
+(homepage-url "http://www.lispme.de/lispme/index_en.html")
+(wikipedia "LispMe")
+(yahoo-group "lispme")
+
+(person "Fred Bayer")
+
+(documentation
+ (title "Manual")
+ (web-url "http://www.lispme.de/lispme/doc/index.htm"))
