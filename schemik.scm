a2dad493802245630479a7d9b3cba773bd4f605e
Lassi Kortela <lassi@lassi.io>
2019-09-10 00:53:56 +0300

    Add Schemik papers

--- schemik.scm
+++ schemik.scm
@@ -1,7 +1,36 @@
 (id schemik)
 (title "Schemik")
 
-(homepage-url "http://schemik.sourceforge.net/")
+(sourceforge "schemik")
+(homepage sourceforge)
+
+(person "Petr Krajca")
+(person "Vilem Vychodil")
+(organization "SUNY Binghamton")
+(organization "Palacký University, Olomouc")
+
+(paper
+ (type inproceedings)
+ (title "Data Parallel Dialect of Scheme: Outline of the Formal Model, Implementation, and Performance")
+ (author "Petr Krajca")
+ (author "Vilem Vychodil")
+ (booktitle "Proceedings of the 2009 ACM symposium on Applied Computing")
+ (year "2009"))
+
+(paper
+ (type inproceedings)
+ (title "Software transactional memory for implicitly parallel functional language")
+ (author "Petr Krajca")
+ (author "Vilem Vychodil")
+ (booktitle "Proceedings of the 2010 ACM Symposium on Applied Computing")
+ (year "2010"))
+
+(paper
+ (type inproceedings)
+ (title "Incremental JIT compiler for implicitly parallel functional language")
+ (author "Petr Krajca")
+ (booktitle "2013 Federated Conference on Computer Science and Information Systems")
+ (year "2013"))
 
 (features
  implicitly-parallel
