9a80d179af38a32bf14530cebf0350c58747f11d
Lassi Kortela <lassi@lassi.io>
2019-09-09 18:46:15 +0300

    Add Jscheme

---
+++ jscheme.scm
@@ -0,0 +1,14 @@
+(id jscheme)
+(title "Jscheme")
+
+(homepage-url "https://norvig.com/jscheme.html")
+(sourceforge "jscheme")
+
+(person "Peter Norvig")
+(person "Tim Hickey")
+(person "Ken Anderson")
+(organization "Brandeis University")
+(organization "BBN Technologies")
+
+(features
+ r4rs)
