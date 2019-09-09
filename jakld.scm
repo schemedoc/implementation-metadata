1fbfaafca76b33d11922df6f710c1174d0b9b146
Lassi Kortela <lassi@lassi.io>
2019-09-09 19:50:02 +0300

    Add JAKLD

---
+++ jakld.scm
@@ -0,0 +1,10 @@
+(id jakld)
+(title "JAKLD")
+(tagline "a Lisp driver to be embedded in Java applications")
+
+(homepage-url "http://www.yuasa.kuis.kyoto-u.ac.jp/~yuasa/jakld/")
+
+(person "Taiichi Yuasa")
+(organization "Kyoto University")
+
+;;; This Scheme implementation was formerly known as "Bubu".
