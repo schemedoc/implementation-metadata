866d44592a29f60b9c94fc28a50921e74cc3b30d
Lassi Kortela <lassi@lassi.io>
2019-10-30 01:51:44 +0200

    Add sxm

---
+++ sxm.scm
@@ -0,0 +1,19 @@
+(id "sxm")
+(title "SXM")
+(alternative-title "CXEMA")
+(tagline "complete R5RS and IEEE Scheme based on XScheme")
+(based-on "xscheme")
+
+(homepage-url "https://malgil.com/sxm/")
+
+(person "Sergei Egorov")
+
+(features
+ syntax-case)
+
+(release
+ (version "1.1")
+ (date "2001")
+ (source-archive
+  (url "https://malgil.com/sxm/sxm-1.1.tar.gz")
+  (hash sha256 "04bf7a4c9b527aa60d85bc074bee95f9a5376586b78fb82c4996f470d640d3da")))
