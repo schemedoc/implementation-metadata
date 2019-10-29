299adcabdc2abc5e9fd3c849065bcf5577252932
Lassi Kortela <lassi@lassi.io>
2019-10-29 19:07:03 +0200

    Add s7

---
+++ s7.scm
@@ -0,0 +1,12 @@
+(id "s7")
+(title "s7")
+(tagline "extension language for Snd, Radium, and Common Music")
+(based-on "tinyscheme")
+
+(homepage-url "https://ccrma.stanford.edu/software/snd/snd/s7.html")
+
+(person "Bill Schottstaedt")
+
+(documentation
+ (title "User Manual")
+ (web-url "https://ccrma.stanford.edu/software/snd/snd/s7.html"))
