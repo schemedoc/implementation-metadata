65a36f275b2a153d8e8ebc92e8ba1b71bf1c43ef
Lassi Kortela <lassi@lassi.io>
2019-11-01 16:04:15 +0200

    Add s7 SourceForge and mailing list

--- s7.scm
+++ s7.scm
@@ -7,6 +7,14 @@
 
 (person "Bill Schottstaedt")
 
+(sourceforge "snd")
+
 (documentation
  (title "User Manual")
  (web-url "https://ccrma.stanford.edu/software/s7/s7.html"))
+
+(mailing-list
+ (name "cmdist")
+ (list-address "cmdist" "ccrma.stanford.edu")
+ (archive-url "https://cm-mail.stanford.edu/mailman/private/cmdist/")
+ (instructions-url "https://cm-mail.stanford.edu/mailman/listinfo/cmdist"))
