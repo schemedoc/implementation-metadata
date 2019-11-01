b9daae24c2306f129efdc5492fdd99a89ae1ac04
Lassi Kortela <lassi@lassi.io>
2019-11-01 15:52:09 +0200

    Update s7 URLs to point to new website

--- s7.scm
+++ s7.scm
@@ -3,10 +3,10 @@
 (tagline "extension language for Snd, Radium, and Common Music")
 (based-on "tinyscheme")
 
-(homepage-url "https://ccrma.stanford.edu/software/snd/snd/s7.html")
+(homepage-url "https://ccrma.stanford.edu/software/s7/")
 
 (person "Bill Schottstaedt")
 
 (documentation
  (title "User Manual")
- (web-url "https://ccrma.stanford.edu/software/snd/snd/s7.html"))
+ (web-url "https://ccrma.stanford.edu/software/s7/s7.html"))
