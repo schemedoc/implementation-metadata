4a219c6dde4157d0cefcbf503170bdadf8d2e953
Göran Weinholt <goran@weinholt.se>
2019-11-10 11:55:33 +0100

    Update loko with recent releases

--- loko.scm
+++ loko.scm
@@ -19,3 +19,23 @@
 (release
  (date 2019-08-22)
  (archive "https://scheme.fail/releases/loko-0.2.1.tar.gz"))
+
+(release
+ (date 2019-09-05)
+ (archive "https://scheme.fail/releases/loko-0.3.0.tar.gz"))
+
+(release
+ (date 2019-09-14)
+ (archive "https://scheme.fail/releases/loko-0.3.1.tar.gz"))
+
+(release
+ (date 2019-09-29)
+ (archive "https://scheme.fail/releases/loko-0.3.2.tar.gz"))
+
+(release
+ (date 2019-10-17)
+ (archive "https://scheme.fail/releases/loko-0.3.3.tar.gz"))
+
+(release
+ (date 2019-11-10)
+ (archive "https://scheme.fail/releases/loko-0.3.4.tar.gz"))
