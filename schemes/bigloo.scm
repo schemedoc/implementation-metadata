4f5ef907b51147c0b11b530e3cccfbb0f3bcab43
Lassi Kortela <lassi@lassi.io>
2020-09-27 16:23:45 +0300

    Update bigloo and add lots of releases

--- schemes/bigloo.scm
+++ schemes/bigloo.scm
@@ -1,8 +1,12 @@
 (title "Bigloo")
 
-(homepage-url "https://www-sop.inria.fr/mimosa/fp/Bigloo/")
-(wikipedia-url "https://en.wikipedia.org/wiki/Bigloo")
-(issue-tracker-url "https://github.com/manuel-serrano/bigloo/issues")
+(repology "bigloo")
+(github "manuel-serrano/bigloo")
+(wikipedia (en "Bigloo"))
+(homepage (url "https://www-sop.inria.fr/mimosa/fp/Bigloo/"))
+(issue-tracker github)
+(version-control github)
+(community-wiki (url "http://bigloo.pbworks.com/w/"))
 
 (person "Manuel Serrano")
 
@@ -10,29 +14,231 @@
  (title "User manual")
  (web-url "https://www-sop.inria.fr/mimosa/fp/Bigloo/doc/bigloo.html"))
 
-(community-wiki-url "http://bigloo.pbworks.com/w/")
-
 (mailing-list
  (name "bigloo")
  (list-address "bigloo" "sophia.inria.fr")
  (archive-url "https://sympa.inria.fr/sympa/arc/bigloo")
  (instructions-url "https://www-sop.inria.fr/mimosa/fp/Bigloo/bigloo.html#Mailing-list"))
 
-(repology "bigloo")
-
-(version-control
- (web-url "https://github.com/manuel-serrano/bigloo")
- (git-url "https://github.com/manuel-serrano/bigloo.git")
- (git-url "git@github.com:manuel-serrano/bigloo.git"))
+(release
+ (version "1.3b")
+ (source-archive
+  (url "ftp://ftp-sop.inria.fr/indes/fp/Bigloo/{scheme-id}{version}.{format}")
+  (archive-prefix "{scheme-id}{version}")
+  (archive-format "tar.bz2")))
 
 (release
+ (version "1.7b"))
+
+(release
+ (version "1.8b"))
+
+(release
+ (version "1.9a"))
+
+(release
+ (version "1.9b"))
+
+(release
+ (version "1.9c"))
+
+(release
+ (version "1.9d"))
+
+(release
+ (version "1.9e"))
+
+(release
+ (version "2.0a"))
+
+(release
+ (version "2.0b"))
+
+(release
+ (version "2.0c"))
+
+(release
+ (version "2.0d"))
+
+(release
+ (version "2.0e"))
+
+(release
+ (version "2.0f"))
+
+(release
+ (version "2.1a"))
+
+(release
+ (version "2.1b"))
+
+(release
+ (version "2.1c"))
+
+(release
+ (version "2.2a"))
+
+(release
+ (version "2.2b"))
+
+(release
+ (version "2.3a"))
+
+(release
+ (version "2.3b"))
+
+(release
+ (version "2.4a"))
+
+(release
+ (version "2.4b"))
+
+(release
+ (version "2.4c")
+ (source-archive
+  (archive-format "tar.gz")))
+
+(release
+ (version "2.4c"))
+
+(release
+ (version "2.5a"))
+
+(release
+ (version "2.5b"))
+
+(release
+ (version "2.5c"))
+
+(release
+ (version "2.6a"))
+
+(release
+ (version "2.6b"))
+
+(release
+ (version "2.6c"))
+
+(release
+ (version "2.6d"))
+
+(release
+ (version "2.6e"))
+
+(release
+ (version "2.6f"))
+
+(release
+ (version "2.7a"))
+
+(release
+ (version "2.8a"))
+
+(release
+ (version "2.8b"))
+
+(release
+ (version "2.8c"))
+
+(release
+ (version "2.9a"))
+
+(release
+ (version "3.0a"))
+
+(release
+ (version "3.0b"))
+
+(release
+ (version "3.0c"))
+
+(release
+ (version "3.1a"))
+
+(release
+ (version "3.1b"))
+
+(release
+ (version "3.2a"))
+
+(release
+ (version "3.2b"))
+
+(release
+ (version "3.3a"))
+
+(release
+ (version "3.4a"))
+
+(release
+ (version "3.5a"))
+
+(release
+ (version "3.6a"))
+
+(release
+ (version "3.7a"))
+
+(release
+ (version "3.8a"))
+
+(release
+ (version "3.8b"))
+
+(release
+ (version "3.8c"))
+
+(release
+ (version "3.9a"))
+
+(release
+ (version "4.0a"))
+
+(release
+ (version "4.0b"))
+
+(release
+ (version "4.1a"))
+
+(release
+ (version "4.2a"))
+
+(release
+ (version "4.2b"))
+
+(release
+ (version "4.2c"))
+
+(release
+ (version "4.3a"))
+
+(release
+ (version "4.3b"))
+
+(release
+ (version "4.3c"))
+
+(release
+ (version "4.3d"))
+
+(release
+ (version "4.3e")
  (date "2018-12-18")
- (version-number "4.3e")
- (source-archive
-  (url "ftp://ftp-sop.inria.fr/indes/fp/Bigloo/bigloo4.3e.tar.gz")))
+ (build-deps "autotools" "gcc" "gmake" "gmp" "unistring")
+ (run-deps "gmp" "unistring")
+ (build-commands "./configure" "make" "make install"))
 
 (release
+ (version "4.3f")
  (date "2019-07-04")
- (version-number "4.3f")
  (source-archive
-  (url "ftp://ftp-sop.inria.fr/indes/fp/Bigloo/bigloo4.3f.tar.gz")))
+  (sha256 "09facb8440ff06edae30b2184a589836f5c598585ef096ca8471e1903e929027")))
+
+(release
+ (version "4.3g"))
+
+(release
+ (version "4.3h")
+ (source-archive
+  (url "ftp://ftp-sop.inria.fr/indes/fp/Bigloo/{scheme-id}-{version}.{format}")
+  (archive-prefix "{scheme-id}-{version}")))
