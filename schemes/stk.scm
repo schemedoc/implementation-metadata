75c1ce5cc1420f560148c1b19606e9cd32a7413d
Lassi Kortela <lassi@lassi.io>
2020-09-28 12:02:01 +0300

    Update stk releases

--- schemes/stk.scm
+++ schemes/stk.scm
@@ -24,18 +24,77 @@
  object-system
  tiny-clos)
 
+;; http://kaolin.unice.fr/STk/news.html
+
 (release
+ (version "2.1.2")
+ (date "1994-09-09"))
+
+(release
+ (version "2.1.3")
+ (date "1994-09-15"))
+
+(release
+ (version "2.1.4")
+ (date "1994-11-09"))
+
+(release
+ (version "2.1.5")
+ (date "1994-12-16"))
+
+(release
+ (version "2.1.6")
+ (date "1995-02-12"))
+
+(release
+ (version "2.1.7")
+ (date "1995-06-05"))
+
+(release
+ (version "2.2.0")
+ (date "1995-07-17"))
+
+(release
+ (version "3.0b1")
+ (date "1995-10-07"))
+
+(release
+ (version "3.0b2")
+ (date "1996-01-21"))
+
+(release
+ (version "3.1.0")
+ (date "1996-07-24"))
+
+(release
+ (version "3.1.1")
  (date "1996-09-26")
- (archive "http://kaolin.unice.fr/STk/Distrib/STk-3.1.1.tar.gz"))
+ (archive "http://kaolin.unice.fr/STk/Distrib/STk-{version}.tar.gz"))
 
 (release
- (date "1999-02-02")
- (archive "http://kaolin.unice.fr/STk/Distrib/STk-3.99.4.tar.gz"))
+ (version "3.99.0")
+ (date "1998-04-10"))
 
 (release
- (date "1999-09-05")
- (archive "http://kaolin.unice.fr/STk/Distrib/STk-4.0.0.tar.gz"))
+ (version "3.99.1")
+ (date "1998-04-30"))
 
 (release
- (date "1999-09-27")
- (archive "http://kaolin.unice.fr/STk/Distrib/STk-4.0.1.tar.gz"))
+ (version "3.99.2")
+ (date "1998-06-09"))
+
+(release
+ (version "3.99.3")
+ (date "1998-09-30"))
+
+(release
+ (version "3.99.4")
+ (date "1999-02-02"))
+
+(release
+ (version "4.0.0")
+ (date "1999-09-05"))
+
+(release
+ (version "4.0.1")
+ (date "1999-09-27"))
