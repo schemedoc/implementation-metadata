08b14f61176a5a8c33615eeae054f461630ec6ec
Lassi Kortela <lassi@lassi.io>
2020-09-28 12:02:50 +0300

    Add stk-ucb-xscheme
    This is a completely different XScheme than the more well-known
    XScheme by Betz which is derived from XLisp. No relation.

---
+++ schemes/stk-ucb-xscheme.scm
@@ -0,0 +1,10 @@
+(title "XScheme (STk)")
+(tagline "graphical Scheme debugger based on STk")
+(based-on "stk")
+
+(university "University of California, Berkeley")
+
+(release
+ (version "0.2")
+ (based-on "stk" "2.1b2")
+ (date "1994"))
