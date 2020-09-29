b4a5cb51f52e717eddc0a68c8166cd9562ff06f2
Lassi Kortela <lassi@lassi.io>
2020-09-30 01:44:59 +0300

    Add s7 general info

--- schemes/s7.scm
+++ schemes/s7.scm
@@ -1,10 +1,12 @@
 (title "s7")
 (tagline "extension language for Snd, Radium, and Common Music")
 (based-on "tinyscheme")
+(named-after "the Sunbeam S7 motorcycle")
 
 (homepage-url "https://ccrma.stanford.edu/software/s7/")
 
 (person "Bill Schottstaedt")
+(country "US")
 
 (sourceforge "snd")
 
