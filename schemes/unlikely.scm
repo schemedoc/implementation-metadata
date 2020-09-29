fdde6398d1fae1d23a8bf1d0b23c7af490093652
Lassi Kortela <lassi@lassi.io>
2020-09-30 01:50:00 +0300

    Add Unlikely Scheme

---
+++ schemes/unlikely.scm
@@ -0,0 +1,8 @@
+(title "Unlikely Scheme")
+(tagline "small, more or less R5RS-compliant interpreter")
+(written-in "C++")
+
+(person "Marijn Haverbeke")
+
+(website "https://marijnhaverbeke.nl/unlikely/")
+(github "marijnh/uscheme")
