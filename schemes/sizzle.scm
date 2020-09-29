cdf332e89ef180805db05a7a0f2a4a4534e318fc
Lassi Kortela <lassi@lassi.io>
2020-09-30 01:43:47 +0300

    Add Sizzle

---
+++ schemes/sizzle.scm
@@ -0,0 +1,17 @@
+(title "Sizzle")
+(tagline "Guile-like, embeddable R5RS interpreter with POSIX and regexp")
+
+(person "Martin Grabmüller")
+(country "DE")
+
+(web (archive-url "20170922034251" "http://catamorph.de/sizzle/sizzle.en.html"))
+(sourceforge "sizzle")
+
+(release
+ (version "0.0.28"))
+
+(release
+ (version "0.0.30"))
+
+(release
+ (version "0.0.31"))
