7a1763a128e12ae11841e18f8830fab66988a06a
Lassi Kortela <lassi@lassi.io>
2020-09-30 12:47:10 +0300

    Split PCS/Geneva from PC-Scheme

---
+++ schemes/pcs-geneva.scm
@@ -0,0 +1,12 @@
+(title "PCS/Geneva")
+(long-title "PC-Scheme/Geneva")
+(based-on "pcscheme")  ; Based on TI PC-Scheme 3.03
+
+(person "Larry Bartholdi")
+(person "Marc Vuilleumier")
+(group "C.U.I. - Scheme group")
+(academy "University of Geneva, Switzerland")
+(country "CH")
+
+(release
+ (version "4.02PL1"))
--- schemes/pcscheme.scm
+++ schemes/pcscheme.scm
@@ -2,14 +2,11 @@
 (title "PC-SCHEME")
 (alternative-title "TI PC-SCHEME")
 (alternative-title "Ibuki PC-SCHEME")
-(alternative-title "PCS/Geneva")
 
 (company "Texas Instruments")
 (company "Ibuki")
-(academy "University of Geneva, Switzerland")
 
 (person "Richard W. Weyhrauch")
-(person "Larry Bartholdi")
 
 (features
  r3rs)
