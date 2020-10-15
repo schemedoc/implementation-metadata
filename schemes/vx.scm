79932458156d04f69d82695e82dc9db81a621df9
Lassi Kortela <lassi@lassi.io>
2020-10-15 19:41:51 +0300

    Update Vx-Scheme

--- schemes/vx.scm
+++ schemes/vx.scm
@@ -1,4 +1,5 @@
 (title "Vx-Scheme")
 (person "Colin Smith")
-(website (url "http://colin-smith.net/vx-scheme/"))
+(written-in "C++")
+(web (archive-url "20120723013430" "http://www.colin-smith.net/vx-scheme/"))
 (google-code "vx-scheme")
