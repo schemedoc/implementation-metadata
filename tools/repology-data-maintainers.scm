a1ec2db0f4cf0711cc2be04eca813fa8e5abcd36
Lassi Kortela <lassi@lassi.io>
2020-12-23 01:34:23 +0200

    Add repology-data-maintainers tool

---
+++ tools/repology-data-maintainers.scm
@@ -0,0 +1,51 @@
+#! /usr/bin/env gosh
+
+(import (scheme base) (scheme char) (scheme file)
+        (scheme read) (scheme write))
+(import (srfi 1) (srfi 132))
+(import (srfi 69))
+
+(define (disp . xs) (for-each display xs) (newline))
+
+(define (alist-ref/default alist key default)
+  (let ((pair (assoc key alist)))
+    (if pair (cdr pair) default)))
+
+(define data (with-input-from-file "repology-data.scm" read))
+
+(define repo->maintainers (make-hash-table))
+
+(for-each
+ (lambda (project)
+   (for-each
+    (lambda (alist)
+      (let ((project-name
+             (car project))
+            (repo-name
+             (alist-ref/default alist "repo" #f))
+            (maintainers
+             (vector->list (alist-ref/default alist "maintainers"
+                                              (vector)))))
+        (for-each (lambda (maintainer)
+                    (hash-table-update!/default
+                     repo->maintainers
+                     repo-name
+                     (lambda (existing)
+                       (list-delete-neighbor-dups
+                        string=?
+                        (list-sort
+                         string-ci<?
+                         (append existing
+                                 (list (string-append project-name " "
+                                                      maintainer))))))
+                     '()))
+                  maintainers)))
+    (vector->list (cdr project))))
+ data)
+(for-each (lambda (project-name)
+            (disp project-name)
+            (for-each (lambda (maintainer)
+                        (disp "  " maintainer))
+                      (hash-table-ref repo->maintainers project-name))
+            (newline))
+          (list-sort string-ci<? (hash-table-keys repo->maintainers)))
