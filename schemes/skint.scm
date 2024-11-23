6b276d3dbddea6d80adcbcd56a218669a1c32e68
retropikzel <retropikzel@iki.fi>
2024-11-23 07:02:17 +0200

    Add tr7 and Skint

---
+++ schemes/skint.scm
@@ -0,0 +1,16 @@
+(short-title "skint")
+(title "skint")
+
+(homepage-url "https://github.com/false-schemers/skint")
+
+(issue-tracker-url "https://github.com/false-schemers/skint/issues")
+(version-control
+  (web-url "https://github.com/false-schemers/skint")
+  (git-url "https://github.com/false-schemers/skint.git"))
+
+(release
+ (date (2024 11 15))
+ (version-number "065")
+ (source-archive
+  (url "https://github.com/false-schemers/skint/archive/refs/tags/v065.tar.gz")
+  (hash sha256 "59bdedb91fb8ee16f8140bdfa184815881f991bf61408c19449dc78071b923d8")))
---
+++ schemes/tr7.scm
@@ -0,0 +1,21 @@
+(short-title "tr7")
+(title "tr7 ")
+
+(homepage-url "https://jobol.gitlab.io/tr7/")
+
+(documentation
+ (title "Documentation")
+ (web-url "https://jobol.gitlab.io/tr7/"))
+
+(issue-tracker-url "https://gitlab.com/jobol/tr7/-/issues")
+(version-control
+  (web-url "https://gitlab.com/jobol/tr7")
+  (git-url "https://gitlab.com/jobol/tr7.git"))
+
+(release
+ (date (2024 11 15))
+ (version-number "2.0.2")
+ (source-archive
+  (url "https://gitlab.com/jobol/tr7/-/archive/v2.0.2/tr7-v2.0.2.tar.gz")
+  (hash sha256 "18645e8bcb6502d99bed1dfc23c4d14d1f65e463205380b18a912e7802a66d76"))
+ (bundled-srfi-implementations 1 69 111 232))
